ee9d4a04f6732cf33c3f2f5028c0f531da1eb51c
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-01-15 19:12:25 -0300

    Ajustando Bild do deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { resolve } from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
+  build: {
+    outDir: 'dist',
   },
-});
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+  },
+})
